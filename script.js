40da80796505c79a2d6771124af187cafffb9068
siraajul <shirajulislamparvez@gmail.com>
2024-11-08 16:49:05 +0600

    Cover--> Basics Of JS

--- script.js
+++ script.js
@@ -1,2 +1,177 @@
-var a; //Declaration
-a = 50; //Initialization
+//Arithmetic Operations
+// var a = 10;
+// var b = 20;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+//alert("Hello");
+// var ans = confirm("Are You");
+// console.log(ans);
+
+// var anss = prompt("Enter Your Name");
+// console.log("User Name is", anss);//String Concat
+
+//Alert---Message(Only)
+//Confirm ---Question
+//Prompt--Take Input
+
+//JS DataTypes
+//-- Permeative
+//---Number
+//---String
+//---Boolean
+//---Undefined
+//---Null
+//---Symbol
+//---Nan
+
+//-- Reference
+///--Array
+///--Objects
+///--Function
+
+// var a = 10;
+// var b = "Sirajul";
+// console.log(a + b);
+
+// if (10 > 50) {
+//   console.log("Correct");
+// } else {
+//   console.log("Not Correct");
+// }
+
+// var ans = prompt("Enter Your Age");
+// if (ans > 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Can't Vote");
+// }
+
+//Loops--Repetation
+// var a = 20;
+// while (a > 0) {
+//   console.log("Hello");
+//   a++;
+// }
+
+// var a = 10;
+// var b = "10";
+// if (a === b) {
+//   console.log("Sahi");
+// } else {
+//   console.log("Galat");
+// }
+// function walk() {
+//   console.log("Good Morning");
+// }
+// function sing() {
+//   console.log("Mei Hun Jiyaan");
+// }
+// function dance() {
+//   console.log("Lets Dance");
+// }
+// walk();
+// sing();
+// dance();
+
+// function greet(a) {
+//   console.log("Good Morning", a);
+// }
+// greet(20);
+// greet(800);
+
+// function abc(){
+//     console.log('Hello')
+//     return 20
+// }
+// var a = abc()
+// console.log(a)
+
+//First Class Function
+// var abc = function () {
+//   console.log("Hello");
+// };
+// abc();
+
+//Fat Arrow Function
+// var bde = () => {
+//   console.log("Hello Fat Arrow Function");
+// };
+// bde();
+
+//-----Let, Const
+// let a = 10; //
+// const b = 20; //
+// var c = 30;
+
+//Array
+// var ar = [20, 30, 40];
+// console.log(ar);
+// var mixed = ["Sirajul", 20, 20.4, true];
+// console.log(mixed[3]);
+
+// var arr = [10, 220, 30, 40, 20];
+// arr.forEach(function (element) {
+//   console.log("Hello", element);
+// });
+
+//--Objects
+
+// var obj = {
+//   user: "Harsh",
+//   age: 20,
+// };
+// console.log(obj.age);
+
+//Function inside object
+// var user = {
+//   userName: "Sarthak",
+// };
+
+//Array Of Object
+// var arr = [
+//   {
+//     userName: "Sirajul",
+//     age: 20,
+//   },
+//   {
+//     userName: "Pavel",
+//     age: 20,
+//   },
+//   {
+//     userName: "Fahim",
+//     age: 20,
+//   },
+// ];
+
+//DOM-Document Object Model
+//4 Pillers of DOM - Selection of an Element
+//--Changing HTML
+//--Changing CSS
+//--Event Listener
+
+//Selecion of an element
+
+// var a = document.querySelector("h1");
+// console.log(a);
+
+//Changing HTML
+// var a = document.querySelector("h1");
+// a.innerHTML = "Changed";
+// console.log(a);
+
+//Changing CSS
+// var a = document.querySelector("h1");
+// a.style.color = "white";
+// a.style.backgroundColor = "red";
+// console.log(a);
+
+//Event Listener
+// var a = document.querySelector("h1");
+// a.addEventListener("mouseenter", function () {
+//   a.innerHTML = "Changed";
+//   a.style.color = "royalblue";
+// });
+// console.log(a);
